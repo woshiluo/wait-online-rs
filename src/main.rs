ffe41465fa44d6d43be31ee40cb36b946d849783
Woshiluo Luo <woshiluo.luo@outlook.com>
2022-01-25 22:34:29 +0800

    fix: file permissions

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,39 @@
-use nix::fcntl;
 use nix::sys::stat;
-use nix::unistd;
+use nix::{fcntl, unistd};
+
+use sd_notify::NotifyState;
 
 fn main() {
     let path = std::path::Path::new("/run/wait-online-rs/204.cond");
 
-    match unistd::mkfifo::<std::path::Path>(&path, stat::Mode::S_IRWXU) {
-        Ok(_) => println!("created {:?}", path),
-        Err(err) => println!("Error creating fifo: {}", err),
+    match unistd::mkfifo::<std::path::Path>(path, stat::Mode::all()) {
+        Ok(_) => (),
+        Err(err) => match err {
+            nix::errno::Errno::EEXIST => {
+                use std::os::unix::fs::FileTypeExt;
+                let metadata = path.metadata().unwrap();
+                if !metadata.file_type().is_fifo() {
+                    panic!("File Exists");
+                }
+            }
+            _ => panic!("Err {}", err),
+        },
     }
 
     loop {
-        let _ = sd_notify::notify(false, &[sd_notify::NotifyState::Status("Checking...")]).unwrap();
+        let _ = sd_notify::notify(false, &[NotifyState::Status("Checking...")]).unwrap();
         match reqwest::blocking::get("https://g.cn/generate_204") {
-            Ok(rep) if rep.status() == 204 => {
-                break;
-            }
+            Ok(rep) if rep.status() == 204 => break,
+            Err(err) => println!("Failed connect: {}", err),
             _ => (),
         }
         std::thread::sleep(std::time::Duration::from_secs(5));
     }
 
-    let _ = sd_notify::notify(false, &[sd_notify::NotifyState::Status("Connected")]).unwrap();
-    let _ = sd_notify::notify(false, &[sd_notify::NotifyState::Ready]).unwrap();
-    let _fd = fcntl::open::<std::path::Path>(
-        &path,
-        fcntl::OFlag::O_RDONLY,
-        nix::sys::stat::Mode::empty(),
-    )
-    .unwrap();
+    let _ = sd_notify::notify(false, &[NotifyState::Status("Connected")]).unwrap();
+    let _ = sd_notify::notify(false, &[NotifyState::Ready]).unwrap();
+    let _fd =
+        fcntl::open::<std::path::Path>(path, fcntl::OFlag::O_RDONLY, stat::Mode::empty()).unwrap();
 
     nix::unistd::pause();
 }
